5055116189040328c747ba3a64a13e81074aee08
miles-fitzsimons <miles.fitzsimons@gmail.com>
2016-09-19 22:19:25 +1200

    refactored get_sum.rb

--- get_sum.rb
+++ get_sum.rb
@@ -1,14 +1,9 @@
 def get_sum(a, b)
-  if a < b
-    return (a..b).reduce(:+)
-  elsif a > b
-    return (b..a).reduce(:+)
-  else
-    return a
-  end
+  a < b ? (a..b).reduce(:+) : (b..a).reduce(:+)
 end
 
 puts get_sum(0, 1)
 puts get_sum(0, -1)
 puts get_sum(1, 2)
 puts get_sum(5, -1)
+puts get_sum(5, 5)
