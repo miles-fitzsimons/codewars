571f407b787222ed84636d571d9a767366f1a451
miles-fitzsimons <miles.fitzsimons@gmail.com>
2016-10-01 17:40:58 +1300

    refactored findOutlier.js

--- findOutlier.js
+++ findOutlier.js
@@ -1,19 +1,9 @@
 var findOutlier = integers => {
-  var oddCount = 0;
-  var evenCount = 0;
-  integers[0] % 2 === 0 ? evenCount++ : oddCount++;
-  integers[1] % 2 === 0 ? evenCount++ : oddCount++;
-  integers[2] % 2 === 0 ? evenCount++ : oddCount++;
-  if (oddCount >= 2) {
-    for (var i = 0; i < integers.length; i++) {
-      if (integers[i] % 2 === 0) return integers[i];
-    }
-  }
-  else {
-    for (var i = 0; i < integers.length; i++) {
-      if (integers[i] % 2 !== 0) return integers[i];
-    }
-  }
+  var even = [], odd = [];
+  integers.forEach(el => {
+    el % 2 === 0 ? even.push(el) : odd.push(el);
+  });
+  return even.length === 1 ? even[0] : odd[0];
 }
 
 console.log(findOutlier([0, 1, 2]), '------  ', 1);
