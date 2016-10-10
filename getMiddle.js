f90fbfd24e7dfe948b8f11950c6e44bdef9f8494
miles-fitzsimons <miles.fitzsimons@gmail.com>
2016-10-10 19:49:11 +1300

    pass getMiddle.js

---
+++ getMiddle.js
@@ -0,0 +1,8 @@
+var getMiddle = s => {
+  return s.length % 2 === 0 ? s[s.length / 2 - 1] + s[s.length / 2] : s[Math.floor(s.length / 2)];
+}
+
+console.log(getMiddle("test"),"es");
+console.log(getMiddle("testing"),"t");
+console.log(getMiddle("middle"),"dd");
+console.log(getMiddle("A"),"A");
