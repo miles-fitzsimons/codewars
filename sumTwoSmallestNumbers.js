1f3c66265c26695608b515f5b5b51f28cd520991
miles-fitzsimons <miles.fitzsimons@gmail.com>
2016-10-16 18:22:25 +1300

    pass sumTwoSmallestNumbers.js

---
+++ sumTwoSmallestNumbers.js
@@ -0,0 +1,10 @@
+var sumTwoSmallestNumbers = numbers => {
+  var arr = numbers.slice(0);
+  return arr.splice(arr.indexOf(Math.min.apply(Math, arr)), 1)[0] + arr.splice(arr.indexOf(Math.min.apply(Math, arr)), 1)[0];
+}
+
+console.log(sumTwoSmallestNumbers([5, 8, 12, 19, 22]), 13 , "Sum should be 13");
+console.log(sumTwoSmallestNumbers([15, 28, 4, 2, 43]), 6 , "Sum should be 6");
+console.log(sumTwoSmallestNumbers([3, 87, 45, 12, 7]), 10 , "Sum should be 10");
+console.log(sumTwoSmallestNumbers([23, 71, 33, 82, 1]), 24 , "Sum should be 24");
+console.log(sumTwoSmallestNumbers([52, 76, 14, 12, 4]), 16 , "Sum should be 16");
